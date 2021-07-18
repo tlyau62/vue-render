7ca7aefb1b630c46f68f1f0c003b91dfdf2ab9d2
Tsz Lam <tyautl@nexify.com.hk>
2021-07-18 15:53:14 +0800

    Add nonfunc

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,20 +1,37 @@
 <script>
 export default {
+  functional: true,
   props: {
     value: {},
     name: {},
   },
-  render() {
-    console.log(this.name);
+  render(h, { props, listeners }) {
+    console.log(props.name);
 
     return (
       <Input
-        value={this.value}
-        onInput={($event) => this.$emit("input", $event.target.value)}
+        value={props.value}
+        onInput={($event) => listeners.input($event.target.value)}
       />
     );
   },
 };
+// export default {
+//   props: {
+//     value: {},
+//     name: {},
+//   },
+//   render() {
+//     console.log(this.name);
+
+//     return (
+//       <Input
+//         value={this.value}
+//         onInput={($event) => this.$emit("input", $event.target.value)}
+//       />
+//     );
+//   },
+// };
 </script>
 
 <style></style>
